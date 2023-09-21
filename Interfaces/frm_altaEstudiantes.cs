142f0d45e2b9514059867a53522ebbe36f3bfd90
Fefoide04 <federicodebrito6@gmail.com>
2023-09-21 11:52:17 -0300

    Subí la solución por primera vez

---
+++ Interfaces/frm_altaEstudiantes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Interfaces
+{
+    public partial class frm_altaEstudiantes : Form
+    {
+        public frm_altaEstudiantes()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_altaEstudiantes_Load(object sender, EventArgs e)
+        {
+            int anio = Convert.ToInt32(DateTime.Now.ToString("yyyy"));
+
+            for (int i = anio - 3; i <= anio; i++)
+            {
+                cbox_anioNacimientoEstudiante.Items.Add(i.ToString());
+            }
+        }
+
+        private void btn_regresar_Click(object sender, EventArgs e)
+        {
+            metodos.cambiarFormulario(metodos.devolverFormularioPorCadena(btn_regresar.Tag.ToString()), variables.panelPrincipal);
+        }
+
+        private void gbox_estudiante_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Interfaces/frm_visualizacionEstudiantes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Interfaces
+{
+    public partial class frm_visualizacionEstudiantes : Form
+    {
+        public frm_visualizacionEstudiantes()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_irConsultas_Click(object sender, EventArgs e)
+        {
+            metodos.cambiarFormulario(metodos.devolverFormularioPorCadena(btn_irConsultas.Tag.ToString()), variables.panelPrincipal);
+        }
+
+        private void btn_agregarEstudiantes_Click(object sender, EventArgs e)
+        {
+            metodos.cambiarFormulario(metodos.devolverFormularioPorCadena(btn_agregarEstudiantes.Tag.ToString()), variables.panelPrincipal);
+        }
+    }
+}
